a9f5c3d753a116596e69f049b3acb515740e262a
julia-marta <marta237@yandex.ru>
2020-07-31 00:33:48 +0300

    Настраивает сборку проекта и сервер, разбивает код на модули

---
+++ src/view/board.js
@@ -0,0 +1,7 @@
+export const createBoardMarkup = () => {
+  return (
+    `<section class="board container">
+        <div class="board__tasks"></div>
+    </section>`
+  );
+};
---
+++ src/view/load-button.js
@@ -0,0 +1,5 @@
+export const createLoadButtonMarkup = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
